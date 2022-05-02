9596770187023dbaa4c7d626ca2146c2ae2bb96d
ge75ceb <ge75ceb@mytum.de>
2022-05-02 21:53:30 +0800

    implement skeleton blueprint for snapshot endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+# import blueprints
+from app.api import bp as api_bp
+
 # Our application
 app = Flask(__name__)
 
 # Load the configuration from config.py
 app.config.from_object('config')
+
+# register blueprints
+app.register_blueprint(api_bp, url_prefix='/api')
 
 # Register the routes
 from app.routes import *
