361d55dd6a5ef2d455e52887e0405a773df143db
Padraig Lamont <padraiglamont@gmail.com>
2022-05-06 14:03:47 +0800

    Revert "Revert Neo4J changes"
    This reverts commit c1b60b712cac5495e08850d94a214b9731be90ad.

--- app/pubmed/sink_db.py
+++ app/pubmed/sink_db.py
@@ -2,6 +2,8 @@
 Allows dumping data into SQLite to speed up iteration
 over the ~33 million records.
 """
+import time
+
 from app.pubmed.model import PubmedCacheConn, Author, Article, ArticleAuthor
 
 
@@ -10,15 +12,21 @@ def add_to_pubmed_cache(conn: PubmedCacheConn, articles: list[Article]):
     Takes in a parsed PubMed data file object, and adds information
     from it to the PubMed cache db.
     """
-    for article in articles:
-        # Create the article.
-        article_id = article.insert(conn)
+    last_report_time = time.time()
+    for index, article in enumerate(articles):
+        with conn.new_transaction() as tx:
+            # Create the article.
+            # article_id = article.insert(conn)
 
-        # Create its authors. Some articles can contain duplicate authors, hence the set.
-        author_relationships = []
-        for author in article.authors:
-            author_id = author.insert(conn)
-            author_relationships.append((article_id, author_id))
+            # Create its authors. Some articles can contain duplicate authors, hence the set.
+            # author_relationships = []
+            for author in article.authors:
+                author.insert(tx)
+                # author_relationships.append((article_id, author_id))
 
-        # Link the authors to the article.
-        ArticleAuthor.insert_many(conn, author_relationships)
+            # Link the authors to the article.
+            # ArticleAuthor.insert_many(conn, author_relationships)
+
+        if index % 100 == 0:
+            print("{} / {}:  {:.3f} seconds".format(index, len(articles), time.time() - last_report_time))
+            last_report_time = time.time()
