4ad4cced7239d9d785e920ecc9391456b7c8ce55
ge75ceb <ge75ceb@mytum.de>
2022-05-06 14:25:37 +0800

    add login endpoint stub

--- app/api/snapshot.py
+++ app/api/snapshot.py
@@ -1,9 +1,12 @@
 from app.api import bp
+from flask import request
 
 @bp.route('/snapshot/visualise/<string:graph_type>', methods=['GET'])
 def visualise_snapshot(graph_type):
+
     return "TODO return {} snapshot".format(graph_type)
 
 @bp.route('/snapshot/analyse', methods=['GET'])
 def analyse_snapshot():
+
     return "TODO return analytics for snapshot"
