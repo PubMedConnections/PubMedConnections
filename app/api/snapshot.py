137645637a56065bd92298c81584d7eebce24f88
moebuta <moebutamail@gmail.com>
2022-05-20 09:07:52 +0800

    add default value to swaggerui.

--- app/api/snapshot.py
+++ app/api/snapshot.py
@@ -1,41 +1,34 @@
-# from app.api import bp
 from flask import request
 from flask_restx import Namespace, Resource, fields
-from app.controller.snapshot_controller import query_by_filters
+from app.controller.snapshot_controller import query_by_filters, retrieve_analytics
 
 ns = Namespace('snapshot', description='snapshot related operations')
 
-publication_date = ns.model('publication_date', {'published_before': fields.String,
-                                                 'published_after': fields.String})
-
 
 filters = ns.model('filters',
-                   {'author': fields.String, 'article_title': fields.String, 'published_before': fields.String,
-                    'published_after': fields.String, 'institution': fields.String, 'journal': fields.String,
-                    'limit': fields.Integer, 'mesh_heading': fields.String})
+                   {'author': fields.String(required=False, default="Annalisa Patrizi"),
+                    'article_title': fields.String(required=False, default=""),
+                    'published_before': fields.String(required=False, default=""),
+                    'published_after': fields.String(required=False, default=""),
+                    'institution': fields.String(required=False, default=""),
+                    'journal': fields.String(required=False, default=""),
+                    'limit': fields.Integer(required=False, default="3"),
+                    'mesh_heading': fields.String(required=False, default="")})
 
 
 @ns.route('/visualise/<string:graph_type>')
 class VisualiseSnapshot(Resource):
     @staticmethod
     @ns.expect(filters)
+    @ns.doc(params={'graph_type':{'description':'graph type: authors/mesh', 'default':'authors'}})
     def post(graph_type):
         filters = request.json
         return query_by_filters(graph_type, filters)
 
 
-@ns.route('/analyse')
+@ns.route('/analyse/<int:snapshot_id>')
 class AnalyseSnapshot(Resource):
     @staticmethod
-    def get():
-        return "TODO return analytics for snapshot"
-
-# @bp.route('/snapshot/visualise/<string:graph_type>', methods=['GET'])
-# def visualise_snapshot(graph_type):
-#
-#     return "TODO return {} snapshot".format(graph_type)
-
-
-# @bp.route('/snapshot/analyse', methods=['GET'])
-# def analyse_snapshot():
-#     return "TODO return analytics for snapshot"
+    @ns.doc(params={'snapshot_id':{'default':'1'}})
+    def get(snapshot_id: int):
+        return retrieve_analytics(snapshot_id)
