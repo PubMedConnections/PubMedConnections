d61d9691346a39676d02b7745d89d0cc03907e02
Jai Castle <22465418@student.uwa.edu.au>
2022-08-19 14:28:16 +0800

    Logout route with expired tokens in DB

--- app/controller/user_controller.py
+++ app/controller/user_controller.py
@@ -1,7 +1,7 @@
+import json
 from neo4j import GraphDatabase, basic_auth
 from config import NEO4J_URI, NEO4J_USER, NEO4J_PASSWORD
 from hashlib import sha256
-import json
 
 def authenticate_user(username: str, password: str) -> bool :
     def cypher(tx):
@@ -23,25 +23,36 @@ def authenticate_user(username: str, password: str) -> bool :
     return len(result) == 1
 
 
-def create_user(username: str, password: str) -> bool:
+def get_user(username: str):
     def get_users(tx):
         return list(tx.run(
             '''
             MATCH (u:User)  
             WHERE u.username = $username
-            RETURN u
+            RETURN properties(u) AS user
             ''',
             {
                 'username': username,
             }
         ))
 
+    driver = GraphDatabase.driver(uri=NEO4J_URI, auth=basic_auth(NEO4J_USER, NEO4J_PASSWORD))
+    session = driver.session()
+    result = session.read_transaction(get_users)
+    if len(result) == 1:
+        return result[0].data()['user']
+    else:
+        return None
+
+
+def create_user(username: str, password: str) -> bool:
     def cypher(tx):
         return list(tx.run(
             '''
             CREATE (u: User 
             {username: $username, 
-            password: $password}
+            password: $password,
+            revoked_tokens: '{}'}
             )
             ''',
             {
@@ -50,11 +61,33 @@ def create_user(username: str, password: str) -> bool:
             }
         ))
 
-    driver = GraphDatabase.driver(uri=NEO4J_URI, auth=basic_auth(NEO4J_USER, NEO4J_PASSWORD))
-    session = driver.session()
-    results = session.read_transaction(get_users)
-    if len(results) > 0:
+    if get_user(username) is not None:
         return False
     else:
+        driver = GraphDatabase.driver(uri=NEO4J_URI, auth=basic_auth(NEO4J_USER, NEO4J_PASSWORD))
+        session = driver.session()
         session.write_transaction(cypher)
         return True
+
+
+def expire_token(username: str, jti: str, expiry: int):
+    user = get_user(username)
+    revoked_tokens = json.loads(user["revoked_tokens"])
+    revoked_tokens[jti] = expiry
+
+    def cypher(tx):
+        return list(tx.run(
+            '''
+            MATCH (u:User {username: $username})
+            SET u.revoked_tokens = $revoked_tokens
+            RETURN properties(u)
+            ''',
+            {
+                'username': username,
+                'revoked_tokens': json.dumps(revoked_tokens)
+            }
+        ))
+
+    driver = GraphDatabase.driver(uri=NEO4J_URI, auth=basic_auth(NEO4J_USER, NEO4J_PASSWORD))
+    session = driver.session()
+    session.write_transaction(cypher)
