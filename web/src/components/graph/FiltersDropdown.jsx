4c1f187f9dfb5f0653761193dcb56b376af8accb
Jai Castle <22465418@student.uwa.edu.au>
2022-09-07 09:47:45 +0800

    Display filters initially, show number when collapsed

--- web/src/components/graph/FiltersDropdown.jsx
+++ web/src/components/graph/FiltersDropdown.jsx
@@ -3,22 +3,20 @@ import Filters from "./Filters";
 import Collapse from '@mui/material/Collapse';
 import Button from '@mui/material/Button';
 import {ArrowDropDown, ArrowDropUp} from '@mui/icons-material'
-
-/*
-TODO:
-* Passthrough starting filters props
-...
- */
+import {useSelector} from 'react-redux'
 
 const FiltersDropdown = () => {
-    const [collapsed, setCollapsed] = useState(true);
+    const activeFilters = useSelector((state) => state.filters.activeFilters);
+    const [collapsed, setCollapsed] = useState(activeFilters.length == 0);
 
     return <div id="filter-dropdown">
         <Collapse in={!collapsed}>
             <Filters />
         </Collapse>
-        <Button id="filter-collapse-button" onClick={() => setCollapsed(!collapsed)} >
-            {collapsed ? <ArrowDropDown /> : <ArrowDropUp />}
+        <Button id="filter-collapse-button"
+                onClick={() => setCollapsed(!collapsed)}
+                startIcon={collapsed ? <ArrowDropDown />: <ArrowDropUp />}>
+            {collapsed ? activeFilters.length + " filter" + (activeFilters.length > 1 ? "s" : "") : ""}
         </Button>
     </div>;
 };
